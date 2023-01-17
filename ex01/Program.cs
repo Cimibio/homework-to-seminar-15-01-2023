1c0c1826cd7d6b9db0f8aea41b0d9553eafd5ed0
AndreySh <shingaryovan@gmail.com>
2023-01-17 22:21:53 +1200

    добавлено первое задание, проверка дня выходной или нет

---
+++ ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите день недели: ");
+int N = int.Parse(Console.ReadLine());
+if ( N == 6 | N == 7)
+{
+    Console.WriteLine("Это выходной день!");
+}
+else if (N <= 0 | N >= 8)
+{
+    Console.WriteLine("Такого дня в неделе нет!");
+}
+else
+{
+    Console.WriteLine("Это обычный, будний день!");
+}
